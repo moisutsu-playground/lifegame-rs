27cf407842bd95998a9064f2003db79171c2be70
moisutsu <moisutsu@gmail.com>
2021-02-04 00:26:22 +0900

    Update pattern

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use lifegame_rs::{LifeGame, State};
 use std::{thread, time};
 
 fn main() {
-    let sleep_time = time::Duration::from_millis(500);
+    let sleep_time = time::Duration::from_millis(100);
     let mut life_game = LifeGame::new(10, 10);
+    life_game.set_cell_state(State::Alive, 4, 5);
     life_game.set_cell_state(State::Alive, 5, 5);
-    life_game.set_cell_state(State::Alive, 5, 6);
+    life_game.set_cell_state(State::Alive, 6, 5);
+    life_game.set_cell_state(State::Alive, 4, 6);
     life_game.set_cell_state(State::Alive, 5, 7);
     loop {
+        print!("{}[2J", 27 as char);
         life_game.display();
         life_game.next();
         thread::sleep(sleep_time);
